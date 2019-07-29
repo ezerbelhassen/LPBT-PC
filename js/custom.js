5cd7a820daf4c4a321268caa06da72d5057914d7
Ezer Belhassen <40840682+ezerbelhassen@users.noreply.github.com>
2019-07-29 18:53:08 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -31,30 +31,9 @@ $(document).ready(function() {
           itemsMobile : [479,1],
       });
     });
-    
-
-    /* Back to Top
-    -----------------------------------------------*/
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 200) {
-          $('.go-top').fadeIn(400);
-            } else {
-                $('.go-top').fadeOut(400);
-           }
-        });   
-          // Animate the scroll to top
-        $('.go-top').click(function(event) {
-          event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 300);
-    });
-
-
+ 
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
 
   });
-$("#submit").click(function(){
-/* Single line Reset function executes on click of Reset Button */
-$("#alert").css('display' , 'block');
-});
--- js/main.js
+++ js/main.js
@@ -1,12 +1,46 @@
-window.onscroll = function() {myFunction()};
+var firebaseConfig = {
+    apiKey: "AIzaSyCtT0thEkML4qHzO34QkdAUcRZP7UvLGrw",
+    authDomain: "lpbtpressclub-5f948.firebaseapp.com",
+    databaseURL: "https://lpbtpressclub-5f948.firebaseio.com",
+    projectId: "lpbtpressclub-5f948",
+    storageBucket: "lpbtpressclub-5f948.appspot.com",
+    messagingSenderId: "884379343766",
+    appId: "1:884379343766:web:fab6392ebc241227"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+var messagesRef = firebase.database().ref();
+
+
+
+document.getElementById('feedbackForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var message = getInputVal('message');
+
+    saveMessage(name, email, message);
+    document.querySelector('#alert').style.display = "block"
+    setTimeout(function(){
+        document.querySelector('#alert').style.display = 'none';
+    },3000)
+}
+
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+function saveMessage(name, email, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
 }
